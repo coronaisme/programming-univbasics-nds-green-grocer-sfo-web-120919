cd9755dbb58e1292d0c622028877f18f098371dc
Nick Corona <coronaisme@gmail.com>
2019-12-04 02:18:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,14 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+   i = 0 
+  while i < collection.length do
+    if name === collection[i][:item]
+      return collection[i]
+    end
+    i += 1
+  end
+  nil
 end
 
 def consolidate_cart(cart)
@@ -9,18 +17,39 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
+  result = []
+   i = 0 
+
+  while i < cart.count do
+    item_name = cart[i][:item]
+    sought_item = find_item_by_name_in_collection(item_name, result)
+    
+    if sought_item
+      sought_item[:count] += 1
+    else
+      cart[i][:count] = 1
+      result.push(cart[i])
+    end
+    i += 1
+  end
+  result
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+   
+   
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
 end
 
 def checkout(cart, coupons)
